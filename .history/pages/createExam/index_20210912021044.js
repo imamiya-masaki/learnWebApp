3509f0ca73a2dd7e07d90a94fe54e9356f06747d
imamiya-masaki <imamiyagit@gmail.com>
2021-09-12 02:16:27 +0900

    init

---
+++ .history/pages/createExam/index_20210912021044.js
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+
+function Home() {
+  return (
+    <ul>
+      <li>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/about">
+          <a>About Us</a>
+        </Link>
+      </li>
+    </ul>
+  );
+}
